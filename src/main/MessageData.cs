2c9440fdf10dd571eac40ea2c99799c189718647
elmerbool <35117496+elmerbool@users.noreply.github.com>
2023-10-11 00:39:57 +1100

    Initial commit

---
+++ src/main/MessageData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ei8.Cortex.Chat.Common
+{
+    public class MessageData
+    {
+        public Guid Id { get; set; }
+
+        public string Content { get; set; }
+
+        public string Sender { get; set; }
+
+        public Guid SenderId { get; set; }
+
+        public string Region { get; set; }
+
+        public Guid? RegionId { get; set; }
+
+        public DateTimeOffset? CreationTimestamp { get; set; }
+
+        public DateTimeOffset? LastModificationTimestamp { get; set; }
+    }
+}
