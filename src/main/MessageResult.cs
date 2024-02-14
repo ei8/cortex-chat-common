90db6b91015a2c83616a7ebfb413a0fe1f3e9eb3
elmerbool <35117496+elmerbool@users.noreply.github.com>
2024-02-14 17:28:56 +1100

    Added MessageResult and RegionResult.

--- src/main/MessageResult.cs
+++ src/main/MessageResult.cs
@@ -1,22 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ei8.Cortex.Chat.Common
 {
-    public class MessageData
+    public class MessageResult
     {
         public Guid Id { get; set; }
 
         public string Content { get; set; }
 
-        public string Sender { get; set; }
+        public string SenderTag { get; set; }
 
         public Guid SenderId { get; set; }
 
-        public string Region { get; set; }
+        public string RegionTag { get; set; }
 
         public Guid? RegionId { get; set; }
+
+        public string ExternalReferenceUrl { get; set; }
 
         public DateTimeOffset? CreationTimestamp { get; set; }
 
---
+++ src/main/RegionResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ei8.Cortex.Chat.Common
+{
+    public class RegionResult
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ExternalReferenceUrl { get; set; }
+        public string Url { get; set; }
+    }
+}
